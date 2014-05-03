bc1f0dd2b84e4b88aa2938857417f7e7a4b5e0d0
johnnyman727 <johnnyman727@gmail.com>
2014-05-03 16:55:27 -0700

    Adding accelerometer licensing information

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ var util = require('util');
 var EventEmitter = require('events').EventEmitter;
 var queue = require('sync-queue');
 
+// Copyright 2014 Technical Machine, Inc. See the COPYRIGHT
+// file at the top-level directory of this distribution.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
 // The SparkFun breakout board defaults to 1, set to 0 if SA0 jumper on the bottom of the board is set
 var I2C_ADDRESS = 0x1D;  // 0x1D if SA0 is high, 0x1C if low
 
