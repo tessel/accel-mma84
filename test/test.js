150530df599bbbc366bb6971cbc36828ed419815
Tim Cameron Ryan <id@timryan.org>
2014-05-14 21:12:28 -0700

    Allow accel functions to be queued before "ready" event is fired.

--- test/test.js
+++ test/test.js
@@ -7,13 +7,11 @@ console.log(t + '..' + tmax);
 var tessel = require('tessel');
 var accel = require('../').use(tessel.port[process.argv[2] || 'A']);
 
-accel.on('ready', function () {
-  accel.on('sample', function (xyz) {
-    ok(Array.isArray(xyz), 'accelerometer data is array');
-    ok(xyz.length == 3, 'three samples');
-    ok(typeof xyz[0] == 'number', 'idx 0 is number');
-    ok(typeof xyz[1] == 'number', 'idx 1 is number');
-    ok(typeof xyz[2] == 'number', 'idx 2 is number');
-    process.exit(0);
-  });
+accel.on('sample', function (xyz) {
+  ok(Array.isArray(xyz), 'accelerometer data is array');
+  ok(xyz.length == 3, 'three samples');
+  ok(typeof xyz[0] == 'number', 'idx 0 is number');
+  ok(typeof xyz[1] == 'number', 'idx 1 is number');
+  ok(typeof xyz[2] == 'number', 'idx 2 is number');
+  process.exit(0);
 });
